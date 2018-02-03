b65b69e37be57fc7c4da6f259a36552a681da092
Kevin <Kevin@DESKTOP-12A507O>
2018-02-03 14:43:26 +0000

    php file test

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE HTML>
+
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>PHP TEST</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <script src="main.js"></script>
+</head>
+<body>
+    <?php
+    echo'<h1>This is a test with pagodabox deployment</h1>' . PHP_EOL;
+    ?>
+</body>
+</html>
